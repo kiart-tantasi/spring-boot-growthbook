55fe55ad6835dd3f86776699942a6509283ccbe7
Petch <petch@Kiarts-MacBook-Air.local>
2024-04-05 15:39:44 +0700

    init spring-boot app

---
+++ api/src/main/java/net/petchblog/growthbook/GrowthbookApplication.java
@@ -0,0 +1,13 @@
+package net.petchblog.growthbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthbookApplication.class, args);
+	}
+
+}

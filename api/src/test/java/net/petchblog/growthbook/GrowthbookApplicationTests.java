c63ebb35b7bf38ac67406394bcb8181a8a1bf390
kiart-tantasi <kiarttantasi@gmail.com>
2024-04-05 17:58:33 +0700

    format api directory with google's checkstyle config

--- api/src/test/java/net/petchblog/growthbook/GrowthbookApplicationTests.java
+++ api/src/test/java/net/petchblog/growthbook/GrowthbookApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GrowthbookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }

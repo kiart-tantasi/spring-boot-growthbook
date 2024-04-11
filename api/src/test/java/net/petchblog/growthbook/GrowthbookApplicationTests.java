a0fc6b84ee8da733a0c74d3316223010ed1c1265
kiart-tantasi <kiarttantasi@gmail.com>
2024-04-11 20:36:43 +0700

    un-comment a test

--- api/src/test/java/net/petchblog/growthbook/GrowthbookApplicationTests.java
+++ api/src/test/java/net/petchblog/growthbook/GrowthbookApplicationTests.java
@@ -1,11 +1,11 @@
 package net.petchblog.growthbook;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GrowthbookApplicationTests {
-  // TODO: mock MySQL so this test can be un-commented
-  //  @Test
-  //  void contextLoads() {
-  //  }
+  @Test
+  void contextLoads() {
+  }
 }
